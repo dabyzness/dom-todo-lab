da06e2dcc6ff56d4dd430b01c7a5127fcef33237
Damian <bzdyrad@gmail.com>
2022-08-30 16:53:07 -0400

    create caches element references

---
+++ js/app.js
@@ -0,0 +1,4 @@
+// Create Cached Element References
+const input = document.getElementById("todo-input");
+const btn = document.getElementById("submit-button");
+const list = document.getElementById("todo-list");
