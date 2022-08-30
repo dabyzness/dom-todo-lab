90c2b60277bec4091ff7a5db882cde7f530ce2b1
Damian <bzdyrad@gmail.com>
2022-08-30 16:58:36 -0400

    create event listener for submit button

--- js/app.js
+++ js/app.js
@@ -2,3 +2,16 @@
 const input = document.getElementById("todo-input");
 const btn = document.getElementById("submit-button");
 const list = document.getElementById("todo-list");
+
+btn.addEventListener("click", handleSubmit);
+
+function handleSubmit(e) {
+  if (!input.value) {
+    return;
+  }
+
+  const item = document.createElement("li");
+  item.textContent = input.value;
+  list.appendChild(item);
+  input.value = "";
+}
